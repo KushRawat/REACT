5f6e73c9f2ae73cfede256756da626100a999dd9
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-07-23 06:51:03 +0530

    ES7

--- JS REFRESHER/classes.js
+++ JS REFRESHER/classes.js
@@ -1,20 +1,17 @@
 class Human {
-  constructor() {
-    this.gender = "male";
-  }
-  printGender() {
+  gender = "male";
+
+  printGender = () => {
     console.log(this.gender);
-  }
+  };
 }
 
 class Person extends Human {
-  constructor() {
-    super();
-    this.name = "Kush";
-  }
-  printName() {
+  name = "Kush";
+
+  printName = () => {
     console.log(this.name);
-  }
+  };
 }
 
 const newPerson = new Person();
